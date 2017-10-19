f1e543574d294a6d90e7d1ce822028e39e42ff36
pongtsu <sittipong.chomchob@gmail.com>
2017-10-19 18:00:07 +0700

    src: Create html and app.

--- contracts/MintToken.sol
+++ contracts/MintToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.15;
 
 contract MintToken {
   mapping (address => uint256) public balanceOf;
@@ -12,5 +12,9 @@ contract MintToken {
     require(balanceOf[_to] + _value >= balanceOf[_to]); // Check for overflows
     balanceOf[msg.sender] -= _value;  // Subtract from the sender
     balanceOf[_to] += _value;  // Add the same to the recipient
+  }
+
+  function getUserBalance(address _address) constant returns (uint256) {
+    return balanceOf[_address];
   }
 }
